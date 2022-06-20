4e001fcbd31520435827948e23b0b06b43ddfd49
Maximilian Rudolph <rudolph-max@gmx.net>
2022-06-21 00:08:49 +0200

    removed licensing - unnecessary

--- isvisible.js
+++ isvisible.js
@@ -1,11 +1,5 @@
 /*
 isvisible.js
-
-Copyright (c) 2022, Maximilian Rudolph
-All rights reserved.
-
-This source code is licensed under the BSD-style license found in the
-LICENSE file in the root directory of this source tree. 
 */
 
 $.fn.isvisible = function() {
